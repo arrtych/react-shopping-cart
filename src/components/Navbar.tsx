6370f7eec1d9e700c06e9990b27a6190295f3516
Artur Lipin <cookiess718@gmail.com>
2024-07-15 03:05:14 +0300

    navbar styling fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { capitalizeFirstLetter } from "../utils/utils";
-import { useState } from "react";
 import "../styles/Navbar.css";
 import CustomShoppingCartIcon from "./CustomShoppingCartIcon";
-import storeItems from "../data/store.json";
 
 interface NavbarProps {
   isOpen: boolean;
@@ -14,18 +11,25 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = (props) => {
-  let navItems = ["home", "store", "about"];
+  const navItems = ["home", "store", "about"];
   const { onClick } = { ...props };
-  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const location = useLocation();
 
   const isHome = (item: string) => {
-    return item == "home";
+    return item === "home";
   };
 
-  // const toggleDrawer = (newOpen: boolean) => () => {
-  //   setOpen(newOpen);
-  // };
-  //todo: fixed menu
+  const isActive = (item: string) => {
+    const currentPath = location.pathname;
+    return isHome(item) ? currentPath === "/" : currentPath.includes(item);
+  };
+
+  const navItemStyle = (isActive: boolean) => ({
+    color: isActive ? "black" : "gray",
+    fontWeight: isActive ? "bold" : "normal",
+    marginRight: "20px",
+  });
+
   return (
     <AppBar position="fixed" style={{ backgroundColor: "white" }}>
       <Toolbar>
@@ -42,23 +46,9 @@ const Navbar: React.FC<NavbarProps> = (props) => {
               variant="h6"
               component={Link}
               key={index}
-              to={isHome(item) ? "/" : "/" + item}
-              style={
-                selectedIndex == index || (isHome(item) && selectedIndex == -1)
-                  ? { color: "black", fontWeight: "bold", marginRight: "20px" }
-                  : { color: "gray", marginRight: "20px" }
-              }
+              to={isHome(item) ? "/" : `/${item}`}
+              style={navItemStyle(isActive(item))}
               className="nav-item"
-              // style={{
-              //   textDecoration: "none",
-              //   marginRight: "20px",
-              // }}
-              // className={
-              //   selectedIndex == index || (isHome(item) && selectedIndex == -1)
-              //     ? "Active"
-              //     : "NotActive"
-              // }
-              onClick={() => setSelectedIndex(index)}
             >
               {capitalizeFirstLetter(item)}
             </Typography>
@@ -67,7 +57,6 @@ const Navbar: React.FC<NavbarProps> = (props) => {
 
         <Button
           variant="outlined"
-          // size="small"
           style={{
             minWidth: "44px",
             width: "44px",
